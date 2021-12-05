4ff2e0f83053629d0217e2fb66302e976705bbaf
Adrien Chapelet <adrien3d@gmail.com>
2021-12-05 19:05:23 +0100

    feat(2021): d5

---
+++ utils/math.go
@@ -0,0 +1,21 @@
+package utils
+
+func Sign(x int) int {
+	if x < 0 {
+		return -1
+	}
+	if x > 0 {
+		return 1
+	}
+	return 0
+}
+
+func Sum(ns []int) int {
+	var sum int
+
+	for _, n := range ns {
+		sum += n
+	}
+
+	return sum
+}
